67d611f3de7ca6943a3ec6fe85362a6f453dc2c3
lol768 <support@technostripe.com>
2012-11-17 21:36:10 +0000

    Create project

--- assets/js/checklist.js
+++ assets/js/checklist.js
@@ -6,6 +6,7 @@ function calc(max)
         if(document.getElementById(i).checked) total+=1;
     }
     total = (total/max)*100;
+    if (total == 100) { $("#button").fadeIn(); } else { $("#button").fadeOut(); }
     document.getElementById('progress').style.cssText = "width: "+total+"%;";
     $('#progress-text').text(Math.round(total)+'% compliance!');
 }
