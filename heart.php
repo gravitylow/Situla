5c78d23994bc89a7bec4d9477e55b73eb145af0d
lol768 <support@technostripe.com>
2012-11-17 16:00:15 +0000

    Add auto link function

--- heart.php
+++ heart.php
@@ -127,4 +127,25 @@ function echoFooter($alerts)
 </html>
     ';
 }
+//Thanks to Eric Coleman
+function auto_link_text($text)
+{
+   $pattern  = '#\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))#';
+   $callback = create_function('$matches', '
+       $url       = array_shift($matches);
+       $url_parts = parse_url($url);
+
+       $text = parse_url($url, PHP_URL_HOST) . parse_url($url, PHP_URL_PATH);
+       $text = preg_replace("/^www./", "", $text);
+
+       $last = -(strlen(strrchr($text, "/"))) + 1;
+       if ($last < 0) {
+           $text = substr($text, 0, $last) . "&hellip;";
+       }
+
+       return sprintf(\'<a rel="nofollow" href="%s">%s</a>\', $url, $text);
+   ');
+
+   return preg_replace_callback($pattern, $callback, $text);
+}
 ?>
